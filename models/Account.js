7d0e12874947c595aed37946050338877cc3ffb5
sd8295642 <sd8295642@gmail.com>
2024-05-22 10:24:44 -0500

    Updated column definitions for Account and Client models

--- models/Account.js
+++ models/Account.js
@@ -4,20 +4,45 @@ const sequelize = require('../config/connection');
 class Account extends Model {}
 
 Account.init (
-    {
-        account_number: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-          },
-        account_type: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+    {   
         client_number: {
             type: DataTypes.INTEGER,
             allowNull: false,
             references: { model: 'client', key: 'client_number' } 
         },
-        account_balance: {}
+        account_number: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+          },
+        // checking, savings, CD, IRA, loan
+        account_type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // single party, joint, or multiple party for personal accounts, not needed for business accounts
+        ownership_type: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        // only for personal and single owner business accounts (Doing Business As/DBA)
+        has_beneficiary: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true
+        },
+        // only for LLCs and Corporations, anyone who owns more than 25% of the entity
+        beneficial_owners: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'account',
     }
-)
+);
+
+module.exports = Account;
--- models/Client.js
+++ models/Client.js
@@ -8,13 +8,49 @@ Client.init(
         client_number: {
             type: DataTypes.INTEGER,
             allowNull: false,
+            unique: true,
             primaryKey: true
         },
-        first_name: {},
-        last_name: {},
-        ssn: {},
-        phone: {},
-        email: {},
-        address: {}
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        entity_name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        tax_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true
+        },
+        phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isEmail: true,
+              }
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'client',
     }
-)
+);
+
+module.exports = Client;
