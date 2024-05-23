7324e46464194e7a5421d54e2da1e197fd851fc1
Ash53rd <aandersonatx@gmail.com>
2024-05-23 13:36:58 -0500

    created homeroute for homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,19 @@
+
+
+const router = require ('express').Router ();
+
+//homepage route
+router.get ('/', async (req,res)=>{
+    try {
+        //if we need info from the database request it here
+
+        //render the view we want to see
+        res.render ('homepage',{
+            //pass data for the handlebars view here
+        }) 
+    } catch (error) {
+    res.status (500).json(error)    
+    }
+})
+
+module.exports = router
