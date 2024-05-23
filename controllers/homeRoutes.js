1d6a67503e23cf05ce3e4baab32551b5290de8a8
Ash53rd <aandersonatx@gmail.com>
2024-05-23 13:39:23 -0500

    created login page route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,5 +15,18 @@ router.get ('/', async (req,res)=>{
     res.status (500).json(error)    
     }
 })
+//login route
+router.get ('/login', async (req,res)=>{
+    try {
+        //if we need info from the database request it here
+
+        //render the view we want to see
+        res.render ('login',{
+            //pass data for the handlebars view here
+        }) 
+    } catch (error) {
+    res.status (500).json(error)    
+    }
+})
 
 module.exports = router
