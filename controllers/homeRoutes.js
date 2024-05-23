d975f1a51b21dbfec43dfe42d4ccd6d6617c7fd0
Ash53rd <aandersonatx@gmail.com>
2024-05-23 13:41:56 -0500

    created workspace route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,5 +28,18 @@ router.get ('/login', async (req,res)=>{
     res.status (500).json(error)    
     }
 })
+//workspace route
+router.get ('/workspace', async (req,res)=>{
+    try {
+        //if we need info from the database request it here
+
+        //render the view we want to see
+        res.render ('workspace',{
+            //pass data for the handlebars view here
+        }) 
+    } catch (error) {
+    res.status (500).json(error)    
+    }
+})
 
 module.exports = router
