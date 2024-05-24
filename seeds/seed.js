9a3bb89dc1d79fa2bf88cefd3a094adf9c3e4b46
Chris Babb <ccbabb@gmail.com>
2024-05-23 21:34:22 -0500

    changed phone number to STRING, fixed paths that weren't working

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,27 +1,27 @@
-const sequelize = require('..config/connection');
-const { User, Client, Account } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Client, Account } = require("../models");
 
-const userData = require('./userData.json');
-const clientData = require('./clientData.json');
-const accountData = require('./accountData.json');
+const userData = require("./userData.json");
+const clientData = require("./clientData.json");
+const accountData = require("./accountData.json");
 
 const seedDatabase = async () => {
-    await sequelize.sync({force: true});
+  await sequelize.sync({ force: true });
 
-    const clients = await Client.bulkCreate(clientData, {
-        individualHooks: true,
-        returning: true,
-      });
+  const clients = await Client.bulkCreate(clientData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-    for (const account of accountData) {
-        await Account.create({
-          ...account,
-          client_number: clients[Math.floor(Math.random() * clients.length)].account_number,
-        });
-      }
-    
-      process.exit(0);
-    
+  for (const account of accountData) {
+    await Account.create({
+      ...account,
+      client_number:
+        clients[Math.floor(Math.random() * clients.length)].account_number,
+    });
+  }
+
+  process.exit(0);
 };
 
 seedDatabase();
