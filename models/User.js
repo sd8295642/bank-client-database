72c138fede5612c36b743b6c0064c59c7d3a17f7
Chris Babb <ccbabb@gmail.com>
2024-05-23 19:52:27 -0500

    added UserRoutes and User model

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection"); // Adjust the path as needed
+const bcrypt = require("bcrypt");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    secret: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+    timestamps: true,
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        if (updatedUserData.changed("password")) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        }
+        return updatedUserData;
+      },
+    },
+  }
+);
+
+module.exports = User;
